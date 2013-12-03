e363edeb8200da6df7b2ca84a0b4ba0f763c31c2
Dave Camp <dave.camp@gmail.com>
2013-12-02 16:16:04 -0800

    Allow projects to be opened by manifest or directory.

--- data/app-manager-mod.js
+++ data/app-manager-mod.js
@@ -1,65 +1,10 @@
-function addPanel() {
-  let button = document.createElement("button");
-  button.setAttribute("class", "button editor-button");
-  button.setAttribute("panel", "editor");
-  button.textContent = "Editor";
-
-  let tabs = document.querySelector("#tabs");
-  tabs.insertBefore(button, tabs.firstChild);
-
-  let frame = document.createElement("iframe");
-  frame.setAttribute("flex", "1")
-  frame.setAttribute("class", "panel editor-panel");
-  frame.setAttribute("src", "chrome://itchpad/content/itchpad.xul");
-  frame.getBoundingClientRect();
-
-  frame.addEventListener("load", (evt) => {
-    return;
-    if (evt.target != frame.contentWindow) {
-      dump("Ignoring: " + evt.target);
-      return;
-    }
-    dump("I CAN MAKE THIS WORK");
-    frame.getBoundingClientRect();
-    try {
-//      let frame = document.querySelector("iframe.editor-panel");
-      dump("element\n");
-      dump(frame);
-      dump("window\n");
-      dump(frame.contentWindow);
-      dump("wrapped\n");
-      dump(frame.contentWindow.wrappedJSObject);
-      dump("init\n");
-      dump(frame.contentWindow.wrappedJSObject.init);
-      dump("calling\n");
-      frame.contentWindow.wrappedJSObject.init();
-      dump("returning\n");
-    } catch(ex) {
-      dump("error: " + ex.stack + "\n")
-      dump(ex);
-    }
-
-  });
-
-  let panels = document.querySelector("#tab-panels");
-  panels.appendChild(frame);
-
-  let init = () => {
-    button.removeEventListener("click", init);
-    frame.contentWindow.wrappedJSObject.init();
-  }
-  button.addEventListener("click", init);
-}
-
-let projects = document.querySelector(".projects-panel");
+const { OS } = Components.utils.import("resource://gre/modules/osfile.jsm", {});
 
 function editClick(thing, location) {
   var wrapper = Components.classes["@mozilla.org/devtools/itchpad;1"].getService(Components.interfaces.nsISupports);
   var service = wrapper.wrappedJSObject;
 
-  dump("EDIT WAS CLICKED: " + thing + ", " + location);
-
-  service.openPath(location);
+  service.openPath(OS.Path.join(location, "manifest.webapp"));
 }
 
 function modifyProjects() {
@@ -70,7 +15,6 @@ function modifyProjects() {
 
     projects.contentWindow.itchpadEditClick = editClick;
 
-    dump("MODIFYING THE PROJECTS WINDOW");
     let doc = projects.contentDocument;
     let buttons = doc.querySelector("#lense-template .project-buttons");
 
@@ -81,8 +25,6 @@ function modifyProjects() {
     edit.textContent = "Edit";
 
     buttons.insertBefore(edit, buttons.firstChild);
-    dump("BUTTONS: " + buttons);
-
   }
 
   projects.addEventListener("load", doModifyProjects);
